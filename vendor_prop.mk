241b3932560a0402498ae55f28a0b3d908d87108
nichcream <c.potato23@gmail.com>
2018-03-10 14:15:09 +0800

    nx531j: Use camera API 1 for VT call
     * Camera API 2 (or HAL3) is known to break video recording in serveral
       apps. Use API 1 for volte video calls just in case.
    
    Change-Id: I9bb3dbe077b092b96afe16a2e75f11a9f9cba0f7

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -114,7 +114,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1
+    persist.dbg.vt_avail_ovr=1 \
+    persist.vendor.qti.telephony.vt_cam_interface=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
