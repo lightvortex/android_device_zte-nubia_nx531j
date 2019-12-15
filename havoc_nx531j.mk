6a9e989698f1a9837f63fec3e7c905d3b30e4049
lightvortex <rishishukla242@gmail.com>
2019-12-15 14:34:42 +0530

    nx531j:track full_common_phone.mk common.mk --> full_common_phone.mk

--- havoc_nx531j.mk
+++ havoc_nx531j.mk
@@ -18,14 +18,12 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit 64-bit configs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx531j/msm8996.mk)
