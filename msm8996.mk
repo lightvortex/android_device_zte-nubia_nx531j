26a7e94bffe188bb09d934d4577bdb84d939398c
lightvortex <rishishukla242@gmail.com>
2020-05-03 11:42:25 +0530

    nx531j:import powerhal 1.3 from pixel

--- msm8996.mk
+++ msm8996.mk
@@ -318,7 +318,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
