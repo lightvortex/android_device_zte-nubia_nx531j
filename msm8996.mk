2a3fc1b3b8ecce2d7ad1e40bb18591cdcffd2194
lightvortex <rishishukla242@gmail.com>
2020-05-10 11:56:40 +0530

    nx531j:update thermals

--- msm8996.mk
+++ msm8996.mk
@@ -385,6 +385,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf 
 
 # USB
 PRODUCT_PACKAGES += \
